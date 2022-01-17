dfc92d5b6bc7792bd411a4e362f59382602e20d6
WinterMeza <75690841+WinterMeza@users.noreply.github.com>
2022-01-17 15:55:22 -0500

    EXAMEN 2DO PARCIAL

---
+++ index.js
@@ -0,0 +1,46 @@
+var getData = function(){
+    var Cedula = document.getElementById("Cedula").value;
+    var Apellidos = document.getElementById("Apellidos").value;
+    var Nombres = document.getElementById("Nombres").value;
+    var Direccion = document.getElementById("Direccion").value;
+    var Semestre = document.getElementById("Semestre").value;
+    var Paralelo = document.getElementById("Paralelo").value;
+    var Correo = document.getElementById("Correo").value;
+    if (Cedula == "") {
+    document.getElementById("Cedula").focus();
+    } else {
+    if (Apellidos == "") {
+    document.getElementById("Apellidos").focus();
+    } else {
+    if (Nombres == "") {
+    document.getElementById("Nombres").focus();
+    } else {
+    if (Direccion == "") {
+    document.getElementById("Direccion").focus();
+    } else {
+    if (Semestre == "") {
+    document.getElementById("Semestre").focus();
+    } 
+    if (Paralelo == "") {
+        document.getElementById("Paralelo").focus();
+        }else
+        if (Correo == "") {
+            document.getElementById("Correo").focus();
+            }
+    else {
+    console.log(Cedula+" "+Apellidos+" "+Nombres+" "+Direccion+" "+Semestre+ ""+Paralelo+""+Correo);
+    document.getElementById("Cedula").value = "";
+    document.getElementById("Apellidos").value = "";
+    document.getElementById("Nombres").value = "";
+    document.getElementById("Direccion").value = "";
+    document.getElementById("Semestre").value = "";
+    document.getElementById("Paralelo").value = "";
+    document.getElementById("Correo").value = "";
+    
+    }
+}
+}
+}
+}
+}
+
